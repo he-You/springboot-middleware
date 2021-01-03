955aa615b6c42e45141ada9dc591d11a9bce20f3
heyou-macbook <heyou_0423@163.com>
2021-01-03 23:13:46 +0800

    add:RocketMQ 消息消费者

---
+++ springboot-rocketmq-consumer/src/main/java/springboot/middleware/rocketmq/RocketMqConsumerApplication.java
@@ -0,0 +1,15 @@
+package springboot.middleware.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/1/3 下午4:51
+ */
+@SpringBootApplication
+public class RocketMqConsumerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(RocketMqConsumerApplication.class, args);
+    }
+}
